878f9f432c9efeffcbc3c43e4edb2292a892ce60
Nu'man Nasyar MZ <numannasyarmz11@gmail.com>
2024-05-04 06:40:55 +0700

    Added a few tables and them fields

--- database/migrations/2024_05_03_232949_create_tables_provinces_cities_transactions_and_transactions_details.php
+++ database/migrations/2024_05_03_232949_create_tables_provinces_cities_transactions_and_transactions_details.php
@@ -11,9 +11,42 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tables_provinces_cities_transactions_and_transactions_details', function (Blueprint $table) {
+        Schema::create('provinces', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
+        });
+
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('province_id')->constrained('provinces')->onDelete('cascade');
+            $table->string('name');
+            $table->timestamps();
+        });
+        
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('province_id')->constrained('provinces')->onDelete('cascade');
+            $table->foreignId('city_id')->constrained('cities')->onDelete('cascade');
+            $table->string('invoice');
+            $table->string('courier_name');
+            $table->string('courier_service');
+            $table->string('courier_cost');
+            $table->integer('weight');
+            $table->string('address');
+            $table->bigInteger('grand_total');
+            $table->string('reference')->nullable();
+            $table->string('status', array("UNPAID", "SUCCESS", "EXPIRED", "CANCELLED"));
+            $table->timestamps();
+        });
+
+        Schema::create('transactions_details', function (Blueprint $table) {
+           $table->id();
+           $table->foreignId('transaction_id')->constrained('transactions')->onDelete('cascade');
+           $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+           $table->integer('quantity');
+           $table->bigInteger('price'); 
         });
     }
 
