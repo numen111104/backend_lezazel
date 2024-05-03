3bbc8a2af219cac05bf4485da86b96c266b760b0
Nu'man Nasyar MZ <numannasyarmz11@gmail.com>
2024-05-04 06:30:47 +0700

    Added a migration

---
+++ database/migrations/2024_05_03_232949_create_tables_provinces_cities_transactions_and_transactions_details.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tables_provinces_cities_transactions_and_transactions_details', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tables_provinces_cities_transactions_and_transactions_details');
+    }
+};
