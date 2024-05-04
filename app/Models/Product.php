f84bc0ba6f296174b02d3fd2ecacbf720bbb9fda
Nu'man Nasyar MZ <numannasyarmz11@gmail.com>
2024-05-04 11:23:42 +0700

    Added 2 forgotten relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'title',
+        'slug',
+        'description',
+        'weight',
+        'discount',
+        'images',
+    ];
+
+    public function category()  
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
 }
