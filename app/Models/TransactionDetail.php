ce3d420c43789202ad5e2e1540c8aaf95e35cdbf
Nu'man Nasyar MZ <numannasyarmz11@gmail.com>
2024-05-05 16:58:04 +0700

    Added  and two relationships

--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class TransactionDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "transaction_id",
+        "product_id",
+        "quantity",
+        "price",
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transaction_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
