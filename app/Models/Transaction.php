91194993637c2d0971837bff74d945985b581982
Nu'man Nasyar MZ <numannasyarmz11@gmail.com>
2024-05-05 16:54:19 +0700

    Added  and four relationships

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,40 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'province_id',
+        'city_id',
+        'invoice',
+        'courier_name',
+        'courier_service',
+        'courier_cost',
+        'weight',
+        'address',
+        'status',
+        'grand_total',
+        'reference'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class, 'province_id');
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class, 'city_id');
+    }
+
+    public function transactionDetails()
+    {
+        return $this->hasMany(TransactionDetail::class, 'transaction_id');
+    }
 }
+
