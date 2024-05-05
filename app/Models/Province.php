ca602439495ace99e969b93e0af1442bf101f884
Nu'man Nasyar MZ <numannasyarmz11@gmail.com>
2024-05-05 16:45:53 +0700

    Added  and one relationship

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Province extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function cities(){
+        return $this->hasMany(City::class);
+    }
 }
